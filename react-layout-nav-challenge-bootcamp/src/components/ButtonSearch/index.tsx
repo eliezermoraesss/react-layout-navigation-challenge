691faf727701a24c344f014f912ddd45bfdb9b3d
eliezermoraesss <eliezer.moraes@outlook.com>
2022-08-01 22:55:19 -0300

    Fixed some ButtonCatalog responsives problems

--- react-layout-nav-challenge-bootcamp/src/components/ButtonSearch/index.tsx
+++ react-layout-nav-challenge-bootcamp/src/components/ButtonSearch/index.tsx
@@ -2,8 +2,14 @@ import './styles.css';
 
 const ButtonSearch = () => {
   return (
-    <div className="input-text-container">
-      <input type="text" className="form-control" placeholder="Digite sua busca"></input>
+    <div className="search-container">
+      <div>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Digite sua busca"
+        ></input>
+      </div>
       <div className="btn-search">
         <button className="btn btn-primary">BUSCAR</button>
       </div>
