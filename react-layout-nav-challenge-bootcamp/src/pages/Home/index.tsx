b9c0eac9c571f9cac60cc7e52964b530f55acb90
eliezermoraesss <eliezer.moraes@outlook.com>
2022-08-02 22:47:08 -0300

    Developing responsivity of all screens - part 3

--- react-layout-nav-challenge-bootcamp/src/pages/Home/index.tsx
+++ react-layout-nav-challenge-bootcamp/src/pages/Home/index.tsx
@@ -9,22 +9,25 @@ const Home = () => {
     <div className="home-container">
       <div className="base-card home-card">
         <div className="home-content-container">
-          <h1>O carro perfeito para você</h1>
-          <p>
-            Conheça nossos carros e dê mais um passo na realização do seu sonho
-          </p>
+          <div>
+            <h1>O carro perfeito para você</h1>
+            <p>
+              Conheça nossos carros e dê mais um passo na realização do seu
+              sonho
+            </p>
+          </div>
         </div>
         <div className="home-image-container">
           <MainImage />
         </div>
       </div>
       <div className="base-card button-content-card">
-        <div className='btn-catalog'>
+        <div className="btn-catalog">
           <Link to="/products">
             <ButtonCatalog />
           </Link>
         </div>
-        <div className='text-catalog'>
+        <div className="text-catalog">
           <p>Comece agora a navegar</p>
         </div>
       </div>
